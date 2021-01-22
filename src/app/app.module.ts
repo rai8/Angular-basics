9e189b5e650d80b7d1b444f16b8ac33fc26387cf
rai8 <hytonne@outlook.com>
2021-01-22 22:31:24 +0300

    working with modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UsersModule } from './users/users.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UsersListComponent],
+  imports: [BrowserModule, AppRoutingModule, UsersModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
