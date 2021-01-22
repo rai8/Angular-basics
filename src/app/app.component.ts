644a73885b693c5f7a86b2f4dc19aad8ce960ba5
rai8 <hytonne@outlook.com>
2021-01-22 22:00:05 +0300

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'beginner';
+  name = 'Krishna Praisley';
+  siteUrl = window.location.href; //get site url
+
+  //using with functions
+  getName() {
+    return 'Shinskey ' + this.name;
+  }
+
+  //using with objects
+  obj = {
+    name: 'Kelly',
+    age: 20,
+  };
+
+  //using with arrays
+  arr = ['Peter', 'Kelly', 'Simon'];
 }
