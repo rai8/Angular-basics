f38e9a2001aeb1d6e3bff75123f01bb58f26e1e6
rai8 <hytonne@outlook.com>
2021-01-23 09:20:01 +0300

    making header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { UsersModule } from './users/users.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, UsersListComponent],
+  declarations: [AppComponent, UsersListComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, UsersModule],
   providers: [],
   bootstrap: [AppComponent],
