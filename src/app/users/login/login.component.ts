dcc452ecd9f4b560419f3d907ab64b510e327c7e
rai8 <hytonne@outlook.com>
2021-01-23 08:59:56 +0300

    creating forms and form value

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  getUsersInfo(val) {
+    console.log(val);
   }
 
+  ngOnInit(): void {}
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   //export the login component
   exports: [LoginComponent, SignupComponent],
 })
